011c225ada1143df42087d4877b4026cabd1bd73
Xursanjonov <nxursanjonov888@gmail.com>
2024-07-16 23:22:15 +0500

    add to modal and user

--- src/pages/customer/Customer.jsx
+++ src/pages/customer/Customer.jsx
@@ -3,16 +3,20 @@ import { useGetCustomersQuery, useUpdateCustomersMutation } from '../../lib/api/
 import TableWrapper from '../../components/table-wrapper';
 import Modal from '../../components/modal/Modal';
 import './customer.scss'
+import Loading from '../../components/loading';
 
 const Customer = () => {
     const [showModal, setShowModal] = useState(false)
     const { data } = useGetCustomersQuery()
     const [update, { data: updateData }] = useUpdateCustomersMutation()
-    
+
     const updateUser = (e) => {
         e.preventDefault()
-        console.log(showModal)
+        console.log(fname)
+        update((user?._id, showModal))
     }
+
+    console.log(updateData)
 
     return (
         <div className='customers'>
@@ -27,17 +31,36 @@ const Customer = () => {
             </ul>
             <ul className="customers-ul">
                 {
-                    data?.innerData?.map((user, inx) => (
-                        <TableWrapper key={user?._id} editUser={updateUser} user={user} id={inx} setEdit={setShowModal} />
-                    ))
+                    data?.innerData ?
+                        data?.innerData?.map((user, inx) => (
+                            <TableWrapper key={user?._id} editUser={updateUser} user={user} id={inx} setEdit={setShowModal} />
+                        )) : <Loading />
                 }
             </ul>
             {
                 showModal ? (
                     <Modal close={setShowModal} key={'1'} title={'Update User'} >
                         <form className="update-form">
-                            <input value={showModal?.fname} type="text" name="" id="" />
-                            <input value={showModal?.lname} type="text" name="" id="" />
+                            <label htmlFor="" className='create-label'>First Name
+                                <input onChange={(e) => setShowModal(p => ({ ...p, fname: e.target.value }))} value={showModal?.fname}
+                                    className='create-input' type="text" />
+                            </label>
+                            <label htmlFor="" className='create-label'>Last Name
+                                <input onChange={(e) => setShowModal(p => ({ ...p, lname: e.target.value }))} value={showModal?.lname}
+                                    className='create-input' type="text" />
+                            </label>
+                            <label htmlFor="" className='create-label'>Address
+                                <input onChange={(e) => setShowModal(p => ({ ...p, address: e.target.value }))} value={showModal?.address}
+                                    className='create-input' type="text" />
+                            </label>
+                            <label htmlFor="" className='create-label'>Phone number
+                                <input onChange={(e) => setShowModal(p => ({ ...p, phone_primary: e.target.value }))} value={showModal?.phone_primary}
+                                    className='create-input' type="text" />
+                            </label>
+                            <label htmlFor="" className='create-label'>Budget
+                                <input onChange={(e) => setShowModal(p => ({ ...p, budget: e.target.value }))} value={showModal?.budget}
+                                    className='create-input' type="text" />
+                            </label>
                             <button type='submit' className='update-btn'>Update</button>
                         </form>
                     </Modal>
